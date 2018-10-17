2af79aaf62651d8a225b2d6d02b393a1b1b52a6a
Vignesh S <s.vignesh@partner.samsung.com>
2018-10-17 17:05:05 +0530

    Basic bundling got working

---
+++ src/ts/main.ts
@@ -0,0 +1,38 @@
+import Images from "../../assets/*.png";
+import { Scene, Game } from 'phaser';
+
+class PlayGame extends Scene {
+  constructor() {
+    super({ key: "PlayGame" });
+  }
+
+  init(data) {
+  }
+
+  preload() {
+    // load images
+    this.load.image("ship", Images["ship"]);
+  }
+
+  create(data) {
+    // render the ship
+    this.ship = this.add.sprite(this.game.config.width/2, this.game.config.height/2, "ship");
+  }
+
+  update(time, delta) {
+  }
+}
+
+window.onload = function() {
+  const config: GameConfig = {
+    width: 320,
+    height: 480,
+    zoom: 2,
+    type: Phaser.AUTO,
+    physics: {
+      default: 'arcade'
+    }
+    scene: [PlayGame]
+  };
+  new Game(config);
+};
